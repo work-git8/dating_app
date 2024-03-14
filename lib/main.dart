09993eb03b3ac584e954b7f4ef354c8a50ed2eea
arijit <arijit0nag@gmail.com>
2024-03-15 02:15:44 +0530

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:dating_app/controller/image_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,7 +9,17 @@ import 'splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Get.put(ImageController());
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+              options: const FirebaseOptions(
+                  apiKey: 'apiKey',
+                  appId: 'appId',
+                  messagingSenderId: 'messagingSenderId',
+                  projectId: 'projectId'))
+          .then((value) {
+          Get.put(ImageController());
+        })
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
